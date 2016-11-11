bfaa0fd856af9e2c78a19c1cc7d9814c838e7609
Brandon Erbschloe <berbschloe@gmail.com>
2016-11-11 17:30:55 -0600

    Update Realm version and gradle tools

--- example/src/main/java/com/quarkworks/android/realmtypesafequery/example/MainActivity.java
+++ example/src/main/java/com/quarkworks/android/realmtypesafequery/example/MainActivity.java
@@ -29,42 +29,43 @@ public class MainActivity extends AppCompatActivity {
         Object f1 = TestRecordFieldNames.DATE_FIELD;
         Object f2 = TestRecordFieldNames.STRING_FIELD;
 
-        RealmConfiguration config = new RealmConfiguration.Builder(getApplicationContext()).deleteRealmIfMigrationNeeded().build();
+        Realm.init(this.getApplicationContext());
+
+        RealmConfiguration config = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
         Realm.setDefaultConfiguration(config);
 
-        Realm realm = Realm.getDefaultInstance();
+        try (Realm realm = Realm.getDefaultInstance()) {
 
-        realm.executeTransaction(new Realm.Transaction() {
-            @Override
-            public void execute(Realm realm) {
-                realm.deleteAll();
-                for (int i = 0; i < 10; i++) {
-                    TestRecord record = realm.createObject(TestRecord.class);
-                    record.booleanField = i % 2 == 0;
-                    record.byteArrayField = new byte[] {(byte) i};
-                    record.byteField = (byte) i;
-                    record.dateField = new Date(i * 1000);
-                    record.doubleField = i * 1000d;
-                    record.floatField = i * 2000f;
-                    record.integerField = i;
-                    record.longField = i * 10L;
-                    record.shortField = (short) i;
-                    record.stringField = i % 3 == 0 ? null :String.valueOf(i);
-                    record.ignoredField = new Object();
-                    record.indexedField = "indexed value: " + i;
-                    record.primaryKey = String.valueOf(i);
-                    record.requiredField = String.valueOf(i);
+            realm.executeTransaction(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+                    realm.deleteAll();
+                    for (int i = 0; i < 10; i++) {
+                        TestRecord record = realm.createObject(TestRecord.class);
+                        record.booleanField = i % 2 == 0;
+                        record.byteArrayField = new byte[]{(byte) i};
+                        record.byteField = (byte) i;
+                        record.dateField = new Date(i * 1000);
+                        record.doubleField = i * 1000d;
+                        record.floatField = i * 2000f;
+                        record.integerField = i;
+                        record.longField = i * 10L;
+                        record.shortField = (short) i;
+                        record.stringField = i % 3 == 0 ? null : String.valueOf(i);
+                        record.ignoredField = new Object();
+                        record.indexedField = "indexed value: " + i;
+                        record.primaryKey = String.valueOf(i);
+                        record.requiredField = String.valueOf(i);
+                    }
                 }
-            }
-        });
+            });
 
-        Log.d(TAG, "Count:" + RealmTypeSafeQuery.where(TestRecord.class).count());
+            Log.d(TAG, "Count:" + RealmTypeSafeQuery.where(TestRecord.class).count());
 
-        Log.d(TAG, "Equal To 1: " + RealmTypeSafeQuery.where(TestRecord.class).equalTo(TestRecordFields.STRING_FIELD, "1").findAll().toString());
-        Log.d(TAG, "Equal To null: " + RealmTypeSafeQuery.where(TestRecord.class).equalTo(TestRecordFields.STRING_FIELD, null).findAll().toString());
-        Log.d(TAG, "IsNull: " + RealmTypeSafeQuery.where(TestRecord.class).isNull(TestRecordFields.STRING_FIELD).findAll().toString());
-        Log.d(TAG, "IsNotNull: " + RealmTypeSafeQuery.where(TestRecord.class).isNotNull(TestRecordFields.STRING_FIELD).findAll().toString());
-
-        realm.close();
+            Log.d(TAG, "Equal To 1: " + RealmTypeSafeQuery.where(TestRecord.class).equalTo(TestRecordFields.STRING_FIELD, "1").findAll().toString());
+            Log.d(TAG, "Equal To null: " + RealmTypeSafeQuery.where(TestRecord.class).equalTo(TestRecordFields.STRING_FIELD, null).findAll().toString());
+            Log.d(TAG, "IsNull: " + RealmTypeSafeQuery.where(TestRecord.class).isNull(TestRecordFields.STRING_FIELD).findAll().toString());
+            Log.d(TAG, "IsNotNull: " + RealmTypeSafeQuery.where(TestRecord.class).isNotNull(TestRecordFields.STRING_FIELD).findAll().toString());
+        }
     }
 }
