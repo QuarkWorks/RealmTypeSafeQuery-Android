b9c9c23f103eeba00d9231828f042b07bcb461f3
Brandon Erbschloe <berbschloe@gmail.com>
2017-10-20 14:27:58 -0500

    Add field skipping

---
+++ annotations/src/main/java/com/quarkworks/android/realmtypesafequery/annotations/SkipGenerationOfRealmField.java
@@ -0,0 +1,14 @@
+package com.quarkworks.android.realmtypesafequery.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by berbschloe on 10/20/17.
+ */
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface SkipGenerationOfRealmField {}
