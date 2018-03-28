a8bee1cce8f2d5469734b28f37ffba0ab2e5a150
James Durham <jamesldurham@gmail.com>
2018-03-28 15:27:10 -0500

    remove nullable and notnull annotations

--- example_kotlinOnly/src/main/java/com/quarkworks/android/realmtypesafequery/example/TestRecord.kt
+++ example_kotlinOnly/src/main/java/com/quarkworks/android/realmtypesafequery/example/TestRecord.kt
@@ -20,52 +20,41 @@ import io.realm.annotations.Required
 @GenerateRealmFields
 class TestRecord : RealmModel {
 
-    @Nullable
     var booleanField: Boolean? = null
 
     internal var boolValue: Boolean = false
 
-    @Nullable
     var byteArrayField: ByteArray? = null
 
-    @Nullable
     var byteField: Byte? = null
 
     var byteValue: Byte = 0
 
-    @Nullable
     var dateField: Date? = null
 
-    @Nullable
     var doubleField: Double? = null
 
     internal var doubleValue: Double = 0.toDouble()
 
-    @Nullable
     var floatField: Float? = null
 
     internal var floatValue: Float = 0.toFloat()
 
-    @Nullable
     var integerField: Integer? = null
 
     var intValue: Int = 0
 
-    @Nullable
     var longField: Long? = null
 
     var longValue: Long = 0
 
-    @Nullable
     var shortField: Short? = null
 
     var shortValue: Short = 0
 
-    @Nullable
     var stringField: String? = null
 
     @Ignore
-    @Nullable
     var ignoredField: Object? = null
 
     @Index
@@ -75,12 +64,9 @@ class TestRecord : RealmModel {
     var primaryKey: String? = null
 
     @Required
-    @NonNull
     var requiredField = RealmDefaults.STRING
 
-    @Nullable
     var parent: TestRecord? = null
 
-    @NonNull
     var children: RealmList<TestRecord> = RealmList()
 }
