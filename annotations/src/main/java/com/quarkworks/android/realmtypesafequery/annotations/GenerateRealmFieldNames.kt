09f95f55f0d953f2feea791cef1cb571fb21fb1c
Paul Gillis <pauldg7@gmail.com>
2018-08-30 10:03:34 -0500

    Converted Annotations to be kotlin

---
+++ annotations/src/main/java/com/quarkworks/android/realmtypesafequery/annotations/GenerateRealmFieldNames.kt
@@ -0,0 +1,9 @@
+package com.quarkworks.android.realmtypesafequery.annotations
+
+/**
+ * @author paul@quarkworks.co (Paul Gillis)
+ */
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
+@Retention(AnnotationRetention.SOURCE)
+annotation class GenerateRealmFieldNames
---
+++ annotations/src/main/java/com/quarkworks/android/realmtypesafequery/annotations/SkipGenerationOfRealmField.kt
@@ -0,0 +1,10 @@
+package com.quarkworks.android.realmtypesafequery.annotations
+
+/**
+ * Created by berbschloe on 10/20/17.
+ * @author paul@quarkworks.co (Paul Gillis)
+ */
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.SOURCE)
+annotation class SkipGenerationOfRealmField
